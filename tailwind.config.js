200deb5cbd311195181f110288fdd53a903bf7cb
Xian L <xnslx89@gmail.com>
2025-01-15 13:47:24 -0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-import formsPlugin from '@tailwindcss/forms';
-import typographyPlugin from '@tailwindcss/typography';
-
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
-  plugins: [formsPlugin, typographyPlugin],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
